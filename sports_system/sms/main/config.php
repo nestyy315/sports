e506f05226f69029e82fadc928aab6bfc73f372b
Nesty Omongos <HZ202301549@wmsu.edu.ph>
2024-12-24 09:44:20 +0800

    Delete sports_system directory

--- sports_system/sms/main/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define base URL dynamically based on server variables
-$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', realpath(__DIR__ . '/../')) . '/') . 'main/';
-define('BASE_URL', $baseUrl);
-
-// Define base directory for images
-define('IMAGE_DIR', __DIR__ . '/../../../pictures/events/');
-?>
